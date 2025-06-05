e13272f3bdefc0ee94f03a5dc99ff31a6d1c648f
10done <anubhavtandon6424@gmail.com>
2025-06-06 04:05:14 +0530

    Added Residencies and companies components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import Header from './Components/Header/Header';
 import Extra from './Components/Extra/Extra';
+import Companies from './Components/Companies/Companies';
+import Residencies from './Residencies/Residencies';
 
 function App() {
   return (
     <div className="App">
+      <div>
      <Header/>
      <Extra/>
+    </div>
+    <Companies/>
+    <Residencies/>
     </div>
   );
 }
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,16 +1,17 @@
 import React from 'react'
+import './Header.css'
 
 export default function Header() {
   return (
     <section className="h-wrapper">
-        <div className="flexCenter padding  h-container">
+        <div className="flexCenter paddings innerWidth  h-container">
             <img src="logo.png" alt="logo" width={100} />
-            <div className="h-menu">
+            <div className="flexCenter h-menu">
                 <a href="">Residencies</a>
                 <a href="">Our Values</a>
                 <a href="">Contact Us</a>
                 <a href="">Get Started</a>
-                <button >Contact</button>
+                <button className='button' >Contact</button>
             </div>
         </div>
     </section>
